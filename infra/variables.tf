2968530978aacc83402952c0bafef2a9676a2a9b
jhonnjc15 <jhonatanjcca@gmail.com>
2026-02-13 09:58:27 -0500

    prueba 16

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,9 +3,15 @@ variable "aws_account_id" { type = string }
 
 variable "ecr_repo" { type = string }
 variable "lambda_name" { type = string }
-variable "state_machine_name" { type = string }
-variable "schedule_name" { type = string }
-variable "schedule_expression" { type = string }
+variable "state_machine_name" { 
+  type = string 
+}
+variable "schedule_name" { 
+  type = string 
+}
+variable "schedule_expression" { 
+  type = string 
+}
 
 # Bootstrap: primera vez apunta a latest (luego el workflow lo cambia a sha)
 variable "image_tag" {
