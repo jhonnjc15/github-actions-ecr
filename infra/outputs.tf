90f012426fbaec9f45d70b31272f0d94bb0a22a6
jhonnjc15 <jhonatanjcca@gmail.com>
2026-02-16 17:00:28 -0500

    PRUEBA 4: schedule

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,33 @@
 output "ecr_repo_url" {
   value = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo}"
 }
-output "lambda_name"        { value = aws_lambda_function.fn.function_name }
-output "state_machine_arn"  { value = aws_sfn_state_machine.sm.arn }
-output "schedule_name"      { value = aws_scheduler_schedule.schedule.name }
 
+output "lambda_name" {
+  value = aws_lambda_function.fn.function_name
+}
 
-output "lambda_exec_role_arn" { value = data.aws_iam_role.lambda_exec.arn }
-output "sfn_role_arn"         { value = data.aws_iam_role.sfn_role.arn }
-output "scheduler_role_arn"   { value = data.aws_iam_role.scheduler_role.arn }
+output "state_machine_arn" {
+  value = aws_sfn_state_machine.sm.arn
+}
+
+output "lambda_exec_role_arn" {
+  value = data.aws_iam_role.lambda_exec.arn
+}
+
+output "sfn_role_arn" {
+  value = data.aws_iam_role.sfn_role.arn
+}
+
+output "scheduler_role_arn" {
+  value = data.aws_iam_role.scheduler_role.arn
+}
+
+output "schedule_name" {
+  description = "Nombre del scheduler (null si schedule_enabled=false)"
+  value       = try(aws_scheduler_schedule.schedule[0].name, null)
+}
+
+output "schedule_arn" {
+  description = "ARN del scheduler (null si schedule_enabled=false)"
+  value       = try(aws_scheduler_schedule.schedule[0].arn, null)
+}
